5fdfd7f3730f264b260754c5f8cb2f1b66440257
loxygenk <me@loxygen.dev>
2023-03-18 23:49:46 +0900

    ✏️ bruhhhh

--- src/github/context.ts
+++ src/github/context.ts
@@ -5,7 +5,7 @@ import { IssuesEvent } from "@octokit/webhooks-definitions/schema";
 
 export function getIssueEvent(context: Context): IssuesEvent | undefined {
   dbg(`event name is ${context.eventName}`);
-  if (context.eventName !== "issue") {
+  if (context.eventName !== "issues") {
     return;
   }
 
