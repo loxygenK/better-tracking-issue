3eb703189e71fff1b30e7c6eabdaf1d37a47f169
loxygenk <me@loxygen.dev>
2023-03-18 20:26:12 +0900

    💬 Change a string literal a little bit

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as core from "@actions/core";
 import { greet } from "~/main";
 
 async function main(): Promise<void> {
-  core.notice("Hey, the action is working!");
+  core.notice("Hey, the action is correctly built and working!");
   core.notice(`Let me do some greeting: ${greet()}`);
 }
 
